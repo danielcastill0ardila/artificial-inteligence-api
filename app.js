9d8f762987ab01e428bfdde9ebcea09f444ae78a
jmbl1685 <jmbl1685@hotmail.com>
2018-08-26 15:29:53 -0500

    FIX: travis

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
-app.use(morgan('dev'))
 app.use(helmet())
 
 require('./routes/file.routes').default(app)
